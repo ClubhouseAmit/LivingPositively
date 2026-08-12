4f930b2bc0b6c672b43d747e912227f9b62ba128
dlibis <thefonz122@gmail.com>
2026-08-12 13:02:27 +0300

    fix test

--- test/MenuTest/reminder_visibility_test.dart
+++ test/MenuTest/reminder_visibility_test.dart
@@ -148,6 +148,7 @@ void main() {
         initialValues: {
           'hasFilled': false,
           'location': '',
+          'customReminder': '',
           'phonePageDataSavedPhoneNames': <String>[],
           'phonePageDataSavedPhoneNumbers': <String>[],
         },
