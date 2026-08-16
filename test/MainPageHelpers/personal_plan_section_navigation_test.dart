303a6ce70958384723a2162dcffa8538412f835f
lubacareer <lubacareer@gmail.com>
2026-08-16 21:47:23 +0300

    test(layout): move SectionHeaderWidget tests to mirrored test/util/layout path and verify default titleKey

--- test/MainPageHelpers/personal_plan_section_navigation_test.dart
+++ test/MainPageHelpers/personal_plan_section_navigation_test.dart
@@ -5,7 +5,6 @@ import 'package:mazilon/MainPageHelpers/components/personal_plan_section.dart';
 import 'package:mazilon/global_enums.dart';
 import 'package:mazilon/pages/home.dart';
 import 'package:mazilon/util/Form/formPagePhoneModel.dart';
-import 'package:mazilon/util/layout/directional_widgets.dart';
 import 'package:mazilon/util/userInformation.dart';
 
 import '../helpers/widget_test_scaffold.dart';
@@ -26,130 +25,6 @@ PhonePageData _phoneData() => PhonePageData(
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  group('SectionHeaderWidget onTitleTap', () {
-    setUp(() {
-      registerTestServices(locale: 'en');
-    });
-
-    tearDown(() {
-      resetTestServices();
-    });
-
-    testWidgets('should trigger onTitleTap callback when title hit target is tapped', (
-      tester,
-    ) async {
-      final handle = tester.ensureSemantics();
-      try {
-        var tapped = false;
-        await pumpWithProviders(
-          tester,
-          Scaffold(
-            body: SectionHeaderWidget(
-              title: 'My Section',
-              titleKey: const Key('customTitleKey'),
-              leadingIcon: Icons.star,
-              onTitleTap: () {
-                tapped = true;
-              },
-            ),
-          ),
-        );
-
-        final titleFinder = find.byKey(const Key('customTitleKey'));
-        expect(titleFinder, findsOneWidget);
-
-        await tester.tap(titleFinder);
-        await tester.pump();
-
-        expect(tapped, isTrue);
-
-        final semantics = tester.getSemantics(find.text('My Section'));
-        expect(semantics.getSemanticsData().hasAction(SemanticsAction.tap), isTrue);
-      } finally {
-        handle.dispose();
-      }
-    });
-
-    testWidgets('should trigger onTitleTap callback when leading icon is tapped', (
-      tester,
-    ) async {
-      var tapped = false;
-      await pumpWithProviders(
-        tester,
-        Scaffold(
-          body: SectionHeaderWidget(
-            title: 'My Section',
-            leadingIcon: Icons.star,
-            onTitleTap: () {
-              tapped = true;
-            },
-          ),
-        ),
-      );
-
-      final iconFinder = find.byIcon(Icons.star);
-      expect(iconFinder, findsOneWidget);
-
-      await tester.tap(iconFinder);
-      await tester.pump();
-
-      expect(tapped, isTrue);
-    });
-
-    testWidgets('should occupy full width of Expanded and be tappable in whitespace', (
-      tester,
-    ) async {
-      var tapCount = 0;
-      await pumpWithProviders(
-        tester,
-        Scaffold(
-          body: SizedBox(
-            width: 400,
-            child: SectionHeaderWidget(
-              title: 'Title',
-              leadingIcon: Icons.star,
-              onTitleTap: () {
-                tapCount++;
-              },
-            ),
-          ),
-        ),
-        surfaceSize: const Size(600, 800),
-      );
-
-      // Tap near the far end of the Expanded region (e.g. at x=250)
-      await tester.tapAt(const Offset(250, 20));
-      await tester.pump();
-
-      expect(tapCount, equals(1));
-    });
-
-    testWidgets('should not wrap with button semantics when onTitleTap is null', (
-      tester,
-    ) async {
-      final handle = tester.ensureSemantics();
-      try {
-        await pumpWithProviders(
-          tester,
-          const Scaffold(
-            body: SectionHeaderWidget(
-              title: 'Static Section',
-              leadingIcon: Icons.star,
-            ),
-          ),
-        );
-
-        final titleFinder = find.text('Static Section');
-        expect(titleFinder, findsOneWidget);
-
-        final semantics = tester.getSemantics(titleFinder);
-        expect(semantics.getSemanticsData().hasAction(SemanticsAction.tap), isFalse);
-      } finally {
-        handle.dispose();
-      }
-    });
-  });
 
   group('PersonalPlanSectionWidget clickable title', () {
     late UserInformation user;
